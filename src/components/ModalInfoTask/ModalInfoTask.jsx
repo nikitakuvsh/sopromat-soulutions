4b2733f31660a96699ea185e9f9937cfb27ab3a4
nikitakuvsh <nikita_kuvshinnikov@inbox.ru>
2024-11-21 12:47:17 +0300

    fix styles

--- src/components/ModalInfoTask/ModalInfoTask.jsx
+++ src/components/ModalInfoTask/ModalInfoTask.jsx
@@ -6,12 +6,23 @@ function ModalInfoTask({ shapes, onClose }) {
         <div className="modal-info-task">
             <div className="modal-info-task__content">
                 <h3 className="modal-info-task__title">Введите значения переменных</h3>
+                <div className="modal-info-task__input-wrapper">
+                    <label className="modal-info-task__label">
+                        E:
+                        <input
+                            className="modal-info-task__input"
+                            type="number"
+                            placeholder="Введите значение"
+                        />
+                        МПа
+                    </label>
+                </div>
                 <ul className="modal-info-task__list">
                     {shapes.map((shape, index) => (
                         <li className="modal-info-task__list-item" key={index}>
                             {shape.type === "square" && (
                                 <>
-                                     A{index + 1}: 
+                                    A{index + 1}: 
                                     <input
                                         className="modal-info-task__input"
                                         type="number"
